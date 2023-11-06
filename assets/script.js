528df64b4a3e7f1121707877b997878b9bd530f1
michael <mikek222@gmail.com>
2023-11-06 15:33:53 -0500

    updated and testing new ways to loop the hours to change CSS colors

--- assets/script.js
+++ assets/script.js
@@ -1,52 +1,57 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
+/// Below: Code will set current day, year, and month to the heading of the page landing.
+
 const SaveBtn = $('.saveBtn')
 var today = dayjs();
 $('#currentDay').text(today.format('MMM D, YYYY'));
-/// Above is the current date formatted for the project
 
+function timeLoop(context) {
+  let currentTime = dayjs().hour()
+  const todos = $(".todo").each(function(i,el){
+    let elId = $(el).attr("id")
+    if (currentTime < elId){
+      // give past class
+    }
+    else if (currentTime == elId){
+      // give current class
+    }
+    else {
+      // give future class
+    }
+  });
+  // let hour = $(context).parent().attr("id")
+  //   if (currentTime === hour) {
+  //     $(this).removeClass("past")
+  //     $(this).removeClass("future")
+  //     $(this).addClass("present")
+  //   }
+  console.log(currentTime)
+  console.log(todos)
+  // console.log(hour)
+}
 
+/// Below: Code will allow the document to fully load before executing, will allow the user to save individual text areas
 $(document).ready(function () {
-
+  let _this = this
   SaveBtn.on('click', function () {
     let write = $(this).siblings('.description').val();
     let hour = $(this).parent().attr("id");
     localStorage.setItem(hour, write)
     console.log(write)
   })
-},
 
+  $("#9 .description").val(localStorage.getItem("9")),
+  $("#10 .description").val(localStorage.getItem("10")),
+  $("#11 .description").val(localStorage.getItem("11")),
+  $("#12 .description").val(localStorage.getItem("12")),
+  $("#13 .description").val(localStorage.getItem("13")),
+  $("#14 .description").val(localStorage.getItem("14")),
+  $("#15 .description").val(localStorage.getItem("15")),
+  $("#16 .description").val(localStorage.getItem("16")),
+  $("#17 .description").val(localStorage.getItem("17")),
 
+  timeLoop(_this)
+});
 
-  // CURRENT PROJECT BELOW - FIGURE OUT TIME LOOP TO ADDRESS COLORS
-  function timeLoop() {
-    let currentTime = dayjs().hour()
-    let hour = $(this).parent().attr("id")
-    if (currentTime === hour) {
-      $(this).removeClass("past")
-      $(this).removeClass("future")
-      $(this).addClass("present")
-    }
-    timeLoop()
-  },
-
-  /// Can use Parse Integer, parseInt() to allow an integer from a string or ID (so i can make a number)
-  /// need to figure out how to seperate all of the IDS
-
-
-
-  $("#hour-9 .description").val(localStorage.getItem("hour-9")),
-  $("#hour-10 .description").val(localStorage.getItem("hour-10")),
-  $("#hour-11 .description").val(localStorage.getItem("hour-11")),
-  $("#hour-12 .description").val(localStorage.getItem("hour-12")),
-  $("#hour-13 .description").val(localStorage.getItem("hour-13")),
-  $("#hour-14 .description").val(localStorage.getItem("hour-14")),
-  $("#hour-15 .description").val(localStorage.getItem("hour-15")),
-  $("#hour-16 .description").val(localStorage.getItem("hour-16")),
-  $("#hour-17 .description").val(localStorage.getItem("hour-17")),
-
-);
 
 
 // Above is the working save button modification
@@ -55,7 +60,7 @@ $(document).ready(function () {
 // TODO: Add a listener for click events on the save button. This code should
 // use the id in the containing time-block as a key to save the user input in
 // local storage. HINT: What does `this` reference in the click listener
-// function? How can DOM traversal be used to get the "hour-x" id of the
+// function? How can DOM traversal be used to get the "x" id of the
 // time-block containing the button that was clicked? How might the id be
 // useful when saving the description in local storage?
 //
