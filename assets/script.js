1e41524dfe267903f0fe1d10b094864393069965
michael <mikek222@gmail.com>
2023-11-06 14:19:55 -0500

    moved code into timeloop, attempting to make POC work so I can expand if statements

--- assets/script.js
+++ assets/script.js
@@ -7,30 +7,34 @@ $('#currentDay').text(today.format('MMM D, YYYY'));
 /// Above is the current date formatted for the project
 
 
-$(document).ready (function () {
+$(document).ready(function () {
 
-  SaveBtn.on('click', function(){
+  SaveBtn.on('click', function () {
     let write = $(this).siblings('.description').val();
     let hour = $(this).parent().attr("id");
     localStorage.setItem(hour, write)
     console.log(write)
-   })
-  },
+  })
+},
 
 
-  
-// CURRENT PROJECT BELOW - FIGURE OUT TIME LOOP TO ADDRESS COLORS
-  function timeLoop (){
-    currentTime = dayjs().hour()
-    if (currentTime === ){
 
+  // CURRENT PROJECT BELOW - FIGURE OUT TIME LOOP TO ADDRESS COLORS
+  function timeLoop() {
+    let currentTime = dayjs().hour()
+    let hour = $(this).parent().attr("id")
+    if (currentTime === hour) {
+      $(this).removeClass("past")
+      $(this).removeClass("future")
+      $(this).addClass("present")
     }
+    timeLoop()
   },
 
-/// Can use Parse Integer, parseInt() to allow an integer from a string or ID (so i can make a number)
-/// need to figure out how to seperate all of the IDS
- 
-  
+  /// Can use Parse Integer, parseInt() to allow an integer from a string or ID (so i can make a number)
+  /// need to figure out how to seperate all of the IDS
+
+
 
   $("#hour-9 .description").val(localStorage.getItem("hour-9")),
   $("#hour-10 .description").val(localStorage.getItem("hour-10")),
@@ -42,38 +46,37 @@ $(document).ready (function () {
   $("#hour-16 .description").val(localStorage.getItem("hour-16")),
   $("#hour-17 .description").val(localStorage.getItem("hour-17")),
 
-  ); 
+);
 
-timeLoop()
 
 // Above is the working save button modification
 
 
-    // TODO: Add a listener for click events on the save button. This code should
-    // use the id in the containing time-block as a key to save the user input in
-    // local storage. HINT: What does `this` reference in the click listener
-    // function? How can DOM traversal be used to get the "hour-x" id of the
-    // time-block containing the button that was clicked? How might the id be
-    // useful when saving the description in local storage?
-    //
+// TODO: Add a listener for click events on the save button. This code should
+// use the id in the containing time-block as a key to save the user input in
+// local storage. HINT: What does `this` reference in the click listener
+// function? How can DOM traversal be used to get the "hour-x" id of the
+// time-block containing the button that was clicked? How might the id be
+// useful when saving the description in local storage?
+//
 
 
 
 
 
-    // TODO: Add code to apply the past, present, or future class to each time
-    // block by comparing the id to the current hour. HINTS: How can the id
-    // attribute of each time-block be used to conditionally add or remove the
-    // past, present, and future classes? How can Day.js be used to get the
-    // current hour in 24-hour time?
-    //
-    // TODO: Add code to get any user input that was saved in localStorage and set
-    // the values of the corresponding textarea elements. HINT: How can the id
-    // attribute of each time-block be used to do this?
-    //
-    // TODO: Add code to display the current date in the header of the page.
+// TODO: Add code to apply the past, present, or future class to each time
+// block by comparing the id to the current hour. HINTS: How can the id
+// attribute of each time-block be used to conditionally add or remove the
+// past, present, and future classes? How can Day.js be used to get the
+// current hour in 24-hour time?
+//
+// TODO: Add code to get any user input that was saved in localStorage and set
+// the values of the corresponding textarea elements. HINT: How can the id
+// attribute of each time-block be used to do this?
+//
+// TODO: Add code to display the current date in the header of the page.
 
 
-  /// Potentially use toggleClass in conjuction with day JS to change colors on the fly for each block?
-  /// Use local storage to save on click event - Can I use the ID of each hour to pull on page load?
-  /// 
+/// Potentially use toggleClass in conjuction with day JS to change colors on the fly for each block?
+/// Use local storage to save on click event - Can I use the ID of each hour to pull on page load?
+/// 
